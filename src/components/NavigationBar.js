a3c6cbd8ae7b89bd523211b95f67bc3f5a540537
charliereinhard <128588074+charliereinhard@users.noreply.github.com>
2023-03-22 18:07:00 +0800

    Add files via upload

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav } from "react-bootstrap"
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <Navbar variant="dark">
+        <Container>
+          <Navbar.Brand href="/">CAiiFILMS</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#trending">TRENDING</Nav.Link>
+            <Nav.Link href="#superhero">SUPERHERO</Nav.Link>
+            <Nav.Link href="#superhero">LINK DOWNLOAD</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar
